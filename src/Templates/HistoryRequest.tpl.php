984d8b9e81c9383086bec8ee5868e8be454aff8a
Paul Barham <paulb@savagesoft.com>
2021-12-05 15:50:51 -0600

    Added History Request

---
+++ src/Templates/HistoryRequest.tpl.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\[[model_uc]];
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class [[model_uc]]HistoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return request()->user()->can('[[route_path]] index');
+    }
+
+    // overwrite parent method to redirect unauthorized requests
+    protected function failedAuthorization()
+    {
+        session()->flash('flash_error_message', "Unauthorized");
+        abort(redirect('/home'));
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [];
+    }
+}
